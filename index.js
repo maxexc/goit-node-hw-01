33b43c08d1e73af1e86e60c646e8eb7ba18cf0e6
Maksym Solomko <max.exc7@gmail.com>
2023-02-19 15:51:52 +0200

    calc

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ console.log(`get current date function result: ${getCurrentDate( )}`);
 
 console.log(process.argv);
 // process.exit();
-console.log(4568);
+// console.log(4568);
 
 // console/log(__dirname);
 
-console.log(new Calc(0.2).sum(0.1).finish());
+const [,, a, b] = process.argv
+
+console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
