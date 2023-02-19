c1692de1a7dc7ec973cf17a7455420d5f6fcbfb3
Maksym Solomko <max.exc7@gmail.com>
2023-02-19 15:34:31 +0200

    test

--- dateUtils.js
+++ dateUtils.js
@@ -2,6 +2,8 @@ function getCurrentDate () {
     return Date.now() + global.testData
 }
 
+// console/log(__filename);
+
 module.exports = {
     getCurrentDate
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Calc = require('calc-js').Calc;
+
 console.log(12123);
 global.testData = 'aaaaaa'
 const {getCurrentDate} = require('./dateUtils')
@@ -7,3 +9,9 @@ console.log(`get current date function result: ${getCurrentDate( )}`);
 // console.log(process.env);
 
 console.log(process.argv);
+// process.exit();
+console.log(4568);
+
+// console/log(__dirname);
+
+console.log(new Calc(0.2).sum(0.1).finish());
